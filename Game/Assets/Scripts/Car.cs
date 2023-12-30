2d48fec99f2f65006335bf79f2152f5c9dec90ae
diaya <cholongyee@gmail.com>
2023-12-30 17:21:19 +0900

    (2020.12.30) 유니티 프로젝트 업로드

---
+++ Game/Assets/Scripts/Car.cs
@@ -0,0 +1,12 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Car : MonoBehaviour
+{
+    [SerializeField]
+    private float speed;
+    [SerializeField]
+    private Vector3 direction;
+
+}
